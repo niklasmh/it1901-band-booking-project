45c68cb79a508c6201e3e621cf99be64780cce9d
Odd Kristian Kvarmestøl <okknor@gmail.com>
2016-09-07 13:53:10 +0200

    Moved template dir to project root

--- band_booking/settings.py
+++ band_booking/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
 	{
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
 		'DIRS': [
-			'band_booking/templates/',
+			os.path.join(BASE_DIR, 'templates/'),
 		],
 		'APP_DIRS': True,
 		'OPTIONS': {
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, 'static/'),
+]
 
 # Try load the settings_local.py file
 def module_exists(module_name):
