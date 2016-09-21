c9c47f5ee2d87f112fd7292ee10ce64ecdb5a03d
Odd Kristian Kvarmestøl <okknor@gmail.com>
2016-09-21 23:44:12 +0200

    Added perms and stateset check to set bookingstate

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, FormView
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from booking import models,forms
 
@@ -19,15 +21,28 @@ class BookingCreateView(PermissionRequiredMixin, CreateView):
 		form.instance.user = self.request.user
 		return super(BookingCreateView, self).form_valid(form);
 
-class BookingSetStateView(FormView):
+class BookingSetStateView(PermissionRequiredMixin, FormView):
+	permission_required = 'booking.accept_booking'
+	form_class = forms.BookingSetStateForm
 
-    form_class = forms.BookingSetStateForm
+	def get(self, request, booking, **kwargs):
+		return HttpResponseRedirect(reverse('booking:detail', kwargs={'booking': booking}))
 
-    def form_valid(self, form):
-        booking = models.Booking.objects.get(id = self.kwargs["booking"])
-        booking.state = "a" if form.cleaned_data["accepted"] else "r"
-        booking.save()
-        return super(BookingSetStateView, self).form_valid(form)
+	def form_valid(self, form):
+		booking = models.Booking.objects.get(id = self.kwargs["booking"])
+		self.object = booking
+		if booking.state != ' ':
+			messages.warning(self.request, 'The booking has already been %s.' % booking.get_state_display())
+			return self.form_invalid(form)
+		booking.state = "a" if form.cleaned_data["accepted"] else "r"
+		booking.save()
+		return super(BookingSetStateView, self).form_valid(form)
 
-    def get_success_url(self):
-        return reverse('booking:list')
+	def form_invalid(self, form):
+		return HttpResponseRedirect(reverse('booking:detail', kwargs={'booking': self.kwargs['booking']}))
+
+	def get_success_url(self):
+		if self.object.state == 'a':
+			return reverse('booking:detail', kwargs={'booking': self.kwargs['booking']})
+		else:
+			return reverse('booking:list')
