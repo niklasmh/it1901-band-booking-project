81eafc0768b1efefb3707a35786905822fd9e730
Odd Kristian Kvarmestøl <okknor@gmail.com>
2016-09-22 01:25:20 +0200

    Implemented replacement bookings

--- booking/models.py
+++ booking/models.py
@@ -4,6 +4,19 @@ from django.contrib.auth.models import User
 from band.models import Band
 from venue.models import Venue
 
+BOOKING_NONE = ' '
+BOOKING_REJECTED = 'r'
+BOOKING_ACCEPTED = 'a'
+BOOKING_CANCELLED = 'c'
+BOOKING_REPLACED = 'm'
+BOOKING_CHOICES = (
+	(BOOKING_NONE, '-'),
+	(BOOKING_REJECTED, 'Rejected'),
+	(BOOKING_ACCEPTED, 'Accepted'),
+	(BOOKING_CANCELLED, 'Cancelled'),
+	(BOOKING_REPLACED, 'Replaced'),
+)
+
 class Booking(models.Model):
 	band = models.ForeignKey(Band)
 	venue = models.ForeignKey(Venue)
@@ -15,9 +28,8 @@ class Booking(models.Model):
 	price = models.DecimalField(max_digits=10, decimal_places=2)
 	created = models.DateTimeField(auto_now_add=True)
 	modified = models.DateTimeField(auto_now=True)
-	state = models.CharField(max_length=1, default=' ', choices=((' ', '-'),
-																 ('r', 'Rejected'),
-																 ('a', 'Accepted')))
+	state = models.CharField(max_length=1, default=' ', choices=BOOKING_CHOICES)
+	replaces = models.ForeignKey('Booking', null=True, blank=True, related_name='replacements')
 
 	def __str__(self):
 		return "%s @ %s" % (self.band, self.venue)
--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, FormView
+from django.views.generic.detail import SingleObjectMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from booking import models,forms
+from django.forms import model_to_dict
+from booking import models, forms
 
 class BookingListView(ListView):
 	model = models.Booking
@@ -21,6 +23,38 @@ class BookingCreateView(PermissionRequiredMixin, CreateView):
 		form.instance.user = self.request.user
 		return super(BookingCreateView, self).form_valid(form);
 
+class BookingUpdateView(PermissionRequiredMixin, CreateView, SingleObjectMixin):
+	permission_required = 'booking.edit_booking'
+	model = models.Booking
+	pk_url_kwarg = 'booking'
+	fields = ('band', 'venue', 'begin', 'end', 'band_fee', 'price_member', 'price')
+
+	def dispatch(self, request, *args, **kwargs):
+		self.object_original = self.get_object()
+		return super(BookingUpdateView, self).dispatch(request, *args, **kwargs)
+
+	def get_initial(self):
+		return model_to_dict(self.object_original)
+
+	def form_valid(self, form):
+		form.instance.replaces = self.object_original
+		form.instance.user = self.request.user
+		res = super(BookingUpdateView, self).form_valid(form)
+		if self.object_original.state == models.BOOKING_NONE:
+			self.object_original.state = models.BOOKING_REPLACED
+			self.object_original.save()
+		return res
+
+	def get_context_data(self, **kwargs):
+		context = {}
+		if self.object_original:
+			context['object'] = self.object_original
+			context_object_name = self.get_context_object_name(self.object_original)
+			if context_object_name:
+				context[context_object_name] = self.object_original
+				context.update(kwargs)
+		return super(BookingUpdateView, self).get_context_data(**context)
+
 class BookingSetStateView(PermissionRequiredMixin, FormView):
 	permission_required = 'booking.accept_booking'
 	form_class = forms.BookingSetStateForm
@@ -34,7 +68,14 @@ class BookingSetStateView(PermissionRequiredMixin, FormView):
 		if booking.state != ' ':
 			messages.warning(self.request, 'The booking has already been %s.' % booking.get_state_display())
 			return self.form_invalid(form)
-		booking.state = "a" if form.cleaned_data["accepted"] else "r"
+		if form.cleaned_data['accepted']:
+			booking.state = models.BOOKING_ACCEPTED
+			replaces = booking.replaces
+			if replaces:
+				replaces.state = models.BOOKING_REPLACED
+				replaces.save()
+		else:
+			booking.state = models.BOOKING_REJECTED
 		booking.save()
 		return super(BookingSetStateView, self).form_valid(form)
 
