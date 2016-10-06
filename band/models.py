76a83c5c694e98b08a6c7de5752851ba81ffb8b4
sikaro <petter.thrane@gmail.com>
2016-10-06 10:29:23 +0200

    some more band migrations

--- band/models.py
+++ band/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Band(models.Model):
 	name = models.CharField(max_length=200)
 	contact_person = models.CharField(max_length=200, blank=True, null=True)
-	contact_phone = models.PositiveIntegerField(max_length=30, blank=True, null=True)
+	contact_phone = models.PositiveIntegerField(blank=True, null=True)
 	contact_email = models.EmailField(max_length=30, blank=True, null=True)
 	spotify_artist_id = models.CharField(max_length=50, blank=True, null=True)
 	active = models.BooleanField(default=True)
